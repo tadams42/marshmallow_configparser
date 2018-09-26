b95cfe47ee2245c00ec33639900280ffd742ff37
Tomislav Adamic <tomislav.adamic@gmail.com>
2018-09-26 13:18:25 +0200

    Bump version: 0.3.3 → 0.4.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name="marshmallow_configparser",
-    version='0.3.3',
+    version='0.4.0',
     license='MIT',
     description="ConfigParser meets marshmallow",
     long_description='%s\n%s' % (
